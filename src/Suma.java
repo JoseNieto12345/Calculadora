04e349d5b8ea7112843604bfd26aa2ff3293398e
Achotla <20243ds180@utez.edu.mx>
2025-09-04 11:39:21 -0600

    achotla

--- src/Suma.java
+++ src/Suma.java
@@ -1,2 +1,30 @@
-public class Suma {
+public class Suma{
+    private int num1;
+    private int num2;
+    public Suma(int num1, int num2){
+        this.num1=num2;
+        this.num2=num1;
+
+    }
+    public int result(){
+        return num1+num2;
+
+    }
+    public int getNum1(){
+        return num2;
+    }
+    public int getNum2(){
+        return num2;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
