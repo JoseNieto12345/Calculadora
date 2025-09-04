2037eddc51ca3d55ee180de8ff7b169d0251ed51
CA3-SEP <202043ds117@utez.edu.mx>
2025-09-04 11:16:29 -0600

    Agregar el funcionamiento de Multiplicar

--- src/Multiplicacion.java
+++ src/Multiplicacion.java
@@ -1,2 +1,12 @@
+
 public class Multiplicacion {
+    private double resultado;
+
+    public double operar(double a, double b) {
+        setResultado(a * b);
+        return getResultado();
+    }
+
+    private double getResultado() { return resultado; }
+    private void setResultado(double resultado) { this.resultado = resultado; }
 }
