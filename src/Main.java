074b05f9da72824dfcdcb6630e58cb3ac59af2db
La garrocha <20243ds117@utez.edu.mx>
2025-09-04 11:08:01 -0600

    cambio en el main

--- src/Main.java
+++ src/Main.java
@@ -19,102 +19,28 @@ public class Main {
 
             switch (opcion){
                 case 1:
-                    System.out.println("Esfera");
-                    System.out.print("Ingresa el radio de la esfera: ");
-                    double radio = sc.nextDouble();
-                    Esfera esfera = new Esfera(radio);
+                    System.out.println("Suma");
+                    Suma suma = new Suma();
 
-                    System.out.println("1 - Calcular Área");
-                    System.out.println("2 - Calcular Volumen");
-                    System.out.print("Elige una opción: ");
-                    int opcionEsfera = sc.nextInt();
 
-                    switch (opcionEsfera) {
-                        case 1:
-                            System.out.println("Área de la esfera: " + esfera.calcularArea());
-                            break;
-                        case 2:
-                            System.out.println("Volumen de la esfera: " + esfera.calcularVolumen());
-                            break;
-                        default:
-                            System.out.println("Opción no válida para esfera.");
-                    }
                     break;
                 case 2:
-                    System.out.println("cubo");
-                    System.out.print("Ingresa el lado del cubo: ");
-                    double lado = sc.nextDouble();
-                    Cubo cubo = new Cubo(lado);
+                    System.out.println("Resta");
 
-                    System.out.println("1 - Calcular Área");
-                    System.out.println("2 - Calcular Volumen");
-                    System.out.print("Elige una opción: ");
-                    int opcionCubo = sc.nextInt();
+                    Resta resta = new Resta();
 
-                    switch (opcionCubo) {
-                        case 1:
-                            System.out.println("Área del cubo: " + cubo.calcularArea());
-                            break;
-                        case 2:
-                            System.out.println("Volumen del cubo: " + cubo.calcularVolumen());
-                            break;
-                        default:
-                            System.out.println("Opción no válida para cubo.");
-                    }
+
                     break;
                 case 3:
-                    System.out.println("Pirámide");
-                    System.out.print("Ingresa la base de la pirámide: ");
-                    float base = sc.nextFloat();
-                    System.out.print("Ingresa la altura de la pirámide: ");
-                    float altura = sc.nextFloat();
-                    System.out.print("Ingresa la altura lateral de la pirámide: ");
-                    float alturaLateral = sc.nextFloat();
+                    System.out.println("Multiplicacion");
+                    Multiplicacion multiplicacion = new Multiplicacion();
 
-                    Piramide piramide = new Piramide(base, altura, alturaLateral);
-
-                    System.out.println("1 - Calcular Área");
-                    System.out.println("2 - Calcular Volumen");
-                    System.out.print("Elige una opción: ");
-                    int opcionPiramide = sc.nextInt();
-
-                    switch (opcionPiramide) {
-                        case 1:
-                            System.out.println("Área de la pirámide: " + piramide.calcularArea());
-                            break;
-                        case 2:
-                            System.out.println("Volumen de la pirámide: " + piramide.calcularVolumen());
-                            break;
-                        default:
-                            System.out.println("Opción no válida para pirámide.");
-                    }
                     break;
                 case 4:
-                    System.out.println("Cono");
-                    System.out.print("Ingresa el radio del cono: ");
-                    float radioCono = sc.nextFloat();
-                    System.out.print("Ingresa la altura del cono: ");
-                    float alturaCono = sc.nextFloat();
-                    System.out.print("Ingresa la generatriz del cono: ");
-                    float generatriz = sc.nextFloat();
+                    System.out.println("Division");
+                    Division division = new Division();
 
-                    Cono cono = new Cono(radioCono, alturaCono, generatriz);
 
-                    System.out.println("1 - Calcular Área");
-                    System.out.println("2 - Calcular Volumen");
-                    System.out.print("Elige una opción: ");
-                    int opcionCono = sc.nextInt();
-
-                    switch (opcionCono) {
-                        case 1:
-                            System.out.println("Área del cono: " + cono.calcularArea());
-                            break;
-                        case 2:
-                            System.out.println("Volumen del cono: " + cono.calcularVolumen());
-                            break;
-                        default:
-                            System.out.println("Opción no válida para cono.");
-                    }
                     break;
                 case 0:
                     System.out.println("Saliendo . . .");
@@ -128,4 +54,3 @@ public class Main {
 
 
     }
-}
